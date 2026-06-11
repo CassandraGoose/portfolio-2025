2b96bd47ab9282ee57d8338f97dc13fe71e52028
CassandraGoose <21070575+CassandraGoose@users.noreply.github.com>
2026-06-10 19:25:44 -0600

    update copy for introduction and education

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -1,45 +1,48 @@
-import Card from './Card'
+import Card from "./Card";
 
 function AboutCard() {
   return (
     <Card
       nav={
-        <p className='border-r border-solid border-black justify-center px-4 flex items-center'>
+        <p className="border-r border-solid border-black justify-center px-4 flex items-center">
           ABOUT
         </p>
       }
-      context={<p className='m-4'>Hi, I'm Cass.</p>}
+      context={<p className="m-4">Hi, I'm Cass.</p>}
     >
       <div>
-        <p className='m-4'>
-          I'm a JavaScript developer with a knack for weaving education and code
-          together. I enjoy working throughout the stack and have a particular
-          love for creating front end applications. I believe that coding is a
-          craft and I aim to deliver thoughtful, empathetic, and elegant code.
-          When I'm not crafting code, you can find me running the DenverScript
-          MeetUp, giving talks in the community, or with my dog.
+        <p className="m-4">
+          After nearly a decade of building applications, primarily for the web,
+          I realized that I have all the knowledge and skill I need in order to
+          secure those applications. I have shifted my mindset from "move fast
+          and break things", to "how might someone else break this?". I spent
+          years reviewing code for vulnerabilities, writing reports regarding
+          security for stakeholders, and weighing risks, but never put a name
+          to that drive and always worked within my title of web developer or
+          software developer. Now, I'm aiming to lead the charge on a team as an
+          Application Security Engineer.
         </p>
-        <div className='flex pt-4 pb-8'>
+        <div className="flex pt-4 pb-8">
           <a
-            className='ml-4 px-4 border border-solid border-black flex justify-center items-center shadow-lg'
-            href='https://github.com/CassandraGoose'
-            target='_blank'
-            rel='noopener noreferrer'
+            className="ml-4 px-4 border border-solid border-black flex justify-center items-center shadow-lg"
+            href="https://github.com/CassandraGoose"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             GitHub
           </a>
           <a
-            className='mx-4 px-4 border border-solid border-black flex justify-center items-center shadow-lg'
-            href='https://www.linkedin.com/in/cassandra-rt/'
-            target='_blank'
-            rel='noopener noreferrer'
+            className="mx-4 px-4 border border-solid border-black flex justify-center items-center shadow-lg"
+            href="https://www.linkedin.com/in/cassandra-rt/"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             LinkedIn
           </a>
         </div>
       </div>
     </Card>
-  )
+  );
 }
 
-export default AboutCard
+export default AboutCard;
--- src/components/EducationCard.tsx
+++ src/components/EducationCard.tsx
@@ -1,109 +1,96 @@
-import Card from './Card'
+import Card from "./Card";
 
 function EducationCard() {
   return (
     <Card
       nav={
-        <p className='border-r border-solid border-black justify-center px-4 flex items-center'>
+        <p className="border-r border-solid border-black justify-center px-4 flex items-center">
           EDUCATION & LEARNING
         </p>
       }
       context={
-        <h1 className='m-4'>
-          I've had some formal education and some of that has been on...education.
-          So, I think it's safe to say that I know how to learn. I believe there
-          is value in learning many disciplines and skills - not only technologies
-          and work-related content. As such, I've included information on my
-          personal and continuing education.
+        <h1 className="m-4">
+          I love to learn. I won't bore you by listing all of the courses and
+          books I've read lately, but I will list my formal education.
         </h1>
       }
     >
-      <div className='m-4'>
-        <p className='font-bold'>Formal Education</p>
-        <ul className='m-4 list-disc'>
-          <li className='ml-4 mb-4'>
-            <p className='italic'>Web Development Certificate: 2016-2017</p>
-            <p className='text-lg'>Galvanize - Web Development Immersive</p>
-          </li>
-          <li className='ml-4 mb-4'>
-            <p className='italic'>Bachelor of Music Education, summa cum laude: 2009-2014</p>
-            <p className='text-lg'>University of Northern Colorado</p>
-          </li>
-        </ul>
-        <p className='font-bold m4'>Personal & Professional Courses</p>
-        <ul className='list-disc m-4'>
-          <li className='ml-4 mb-4'>
-            <p className='italic'>Various College Courses</p>
-            <p className='text-lg'>Various</p>
-            <p className='text-lg'>
-              Probability and Statistics, Web Development, Data Structures & Algorithms
+      <div className="m-4">
+        <p className="font-bold">Formal Education</p>
+        <ul className="m-4 list-disc">
+          <li className="ml-4 mb-4">
+            <p className="italic">B.S. in Software Engineering: 2025-2026</p>
+            <p className="text-lg">Western Governor's University</p>
+            <hr />
+            <p>
+              Regardless of my work history, hiring someone without a technical
+              degree is a risk some companies will not make. To mitigate that
+              risk myself, I decided to pursue an accelerated degree path and
+              leveraged my experience to move through coursework quickly. I
+              received multiple excellence awards for my final projects and
+              exemplary marks on my assessments. This university does not allow
+              for differentiated honors when graduating.
             </p>
           </li>
-          <li className='ml-4 mb-4'>
-            <p className='italic'>Functional JavaScript First Steps</p>
-            <p className='text-lg'>FrontEnd Masters</p>
-            <p className='text-lg'>
-              An introduction to functional programming using JavaScript
+          <li className="ml-4 mb-4">
+            <p className="italic">Web Development Certificate: 2016-2017</p>
+            <p className="text-lg">Galvanize - Web Development Immersive</p>
+            <hr />
+            <p>
+              I spent six months spent actively learning with peers covering
+              JavaScript, Node.js, AngularJS, Node.js, SQL, PostgreSQL, and
+              more. I want to be clear that this was not a couple of hours a day
+              sitting in a chair and zoning out. I had my hands on a keyboard
+              for well over 8 hours every day, including weekends.
             </p>
           </li>
-          <li className='ml-4 mb-4'>
-            <p className='italic'>The Hard Parts of Functional JavaScript</p>
-            <p className='text-lg'>FrontEnd Masters</p>
-            <p className='text-lg'>
-              A deep dive into functional concepts using JavaScript
+          <li className="ml-4 mb-4">
+            <p className="italic">
+              Bachelor of Music Education, summa cum laude: 2009-2014
             </p>
-          </li>
-          <li className='ml-4 mb-4'>
-            <p className='italic'>CS50</p>
-            <p className='text-lg'>An Open/Online Course from Harvard</p>
-            <p className='text-lg'>
-              An introduction to the intelectual enterprises of computer science
-              and the art of programming. Includes foundational knowledge of the
-              following tech: C, Python, SQL
-            </p>
-          </li>
-          <li className='ml-4 mb-4'>
-            <p className='italic'>Steps In Japanese 1</p>
-            <p className='text-lg'>An Open/Online Course from Waseda University</p>
-            <p className='text-lg'>Fundamental/Survival Japenese Language Course</p>
-          </li>
-          <li className='ml-4 mb-4'>
-            <p className='italic'>Comics: Art in Relationship</p>
-            <p className='text-lg'>An Open/Online Course from California College of the Arts</p>
-            <p className='text-lg'>
-              An exploration of the context and concepts that tell stories within
-              comics.
+            <p className="text-lg">University of Northern Colorado</p>
+            <hr />
+            <p>
+              I graduated with highest honors and landed on the Dean's List
+              every single semester. I won the Angie Southard Music Aware with
+              my quartet Grenadilla Warefare and completed hundreds of extra
+              hours of teaching field observation. On top of that, I worked as
+              the Bands Librarian, ensuring over 5 full ensembles, totalling
+              hundreds of students, had the appropriate music at all times.
             </p>
           </li>
         </ul>
-        <p className='font-bold m4'>Tech Conferences Attended</p>
-        <ul className='list-disc m-4'>
-          <li className='ml-4 mb-4'>
-            <p className='italic'>React Summit, 2023</p>
-            <p className='text-lg'>
+        <p className="font-bold m4">Tech Conferences Attended</p>
+        <ul className="list-disc m-4">
+          <li className="ml-4 mb-4">
+            <p className="italic">React Summit, 2023</p>
+            <p className="text-lg">
               Remote/Hybrid, multi-track React conference (I attended remotely)
             </p>
           </li>
-          <li className='ml-4 mb-4'>
-            <p className='italic'>JSNation, 2023</p>
-            <p className='text-lg'>
-              Remote/Hybrid, multi-track JavaScript conference (I attended remotely)
+          <li className="ml-4 mb-4">
+            <p className="italic">JSNation, 2023</p>
+            <p className="text-lg">
+              Remote/Hybrid, multi-track JavaScript conference (I attended
+              remotely)
             </p>
           </li>
-          <li className='ml-4 mb-4'>
-            <p className='italic'>Devternity, 2022</p>
-            <p className='text-lg'>A remote, multi-track development conference</p>
+          <li className="ml-4 mb-4">
+            <p className="italic">Devternity, 2022</p>
+            <p className="text-lg">
+              A remote, multi-track development conference
+            </p>
           </li>
-          <li className='ml-4 mb-4'>
-            <p className='italic'>DVLP DNVR 2017, 2018, 2019</p>
-            <p className='text-lg'>
+          <li className="ml-4 mb-4">
+            <p className="italic">DVLP DNVR 2017, 2018, 2019</p>
+            <p className="text-lg">
               A 2 day, in-person, multi-track development and design conference
             </p>
           </li>
         </ul>
       </div>
     </Card>
-  )
+  );
 }
 
-export default EducationCard
+export default EducationCard;
